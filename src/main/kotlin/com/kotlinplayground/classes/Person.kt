bd67cc4ca80822ae4aaaa74810cc4bc77a1e62a3
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-10-05 22:19:32 +0300

    Classes: Basic

---
+++ src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -0,0 +1,12 @@
+package com.kotlinplayground.classes
+
+class Person {
+    fun action() {
+        println("Person Walks")
+    }
+}
+
+fun main() {
+    val person = Person()
+    person.action()
+}
